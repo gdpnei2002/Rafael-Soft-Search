5042d6f80f27c4cd1369d3541aaf92d3b42dc80d
neilson <joseneilson25@gmail.com>
2022-09-17 00:43:58 -0300

    otimização 2

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,11 +1,9 @@
 import { LayoutContainer } from "./styles";
 import { Outlet } from "react-router-dom";
-import { Header } from "../../components/Header";
 
 export function DefaultLayout() {
   return (
     <LayoutContainer>
-      <Header />
       <Outlet />
     </LayoutContainer>
   );
